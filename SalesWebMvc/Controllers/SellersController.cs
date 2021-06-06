6fdabe2acec5752eff236a00dc68c64bbf6455db
gfonseca18 <gercia.fonseca@icloud.com>
2021-06-06 21:21:25 +0100

    Foreign key not null

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
 using SalesWebMvc.Services;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -26,6 +27,14 @@ namespace SalesWebMvc.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]//anottation
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Sellers sellers)
+        {
+            _sellersService.Insert(sellers);
+            return RedirectToAction(nameof(Index));
+
         }
     }
 }
