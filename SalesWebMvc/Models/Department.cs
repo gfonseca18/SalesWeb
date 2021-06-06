f5f151c8a776d0cba8984a762b9ac658bd1b0482
gfonseca18 <gercia.fonseca@icloud.com>
2021-06-06 15:44:18 +0100

    Other entities and second migration

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -1,9 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace SalesWebMvc.Models
 {
     public class Department
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Sellers> Sellers { get; set; } = new List<Sellers>();
+
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public void AddSeller(Sellers sellers)
+        {
+            Sellers.Add(sellers);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(sellers => sellers.TotalSales(initial, final));
+        }
     }
 }
