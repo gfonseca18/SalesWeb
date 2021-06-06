7165b0cea2cb80ae75b271d00127e4ec58f4e290
gfonseca18 <gercia.fonseca@icloud.com>
2021-06-06 21:09:31 +0100

    Create Form

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,8 +11,19 @@ namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellersService _sellersService;
+
+        public SellersController(SellersService sellerService)
+        {
+            _sellersService = sellerService;
+        }
         // GET: /<controller>/
         public IActionResult Index()
+        {
+            var list = _sellersService.FindAll();
+            return View(list);
+        }
+        public IActionResult Create()
         {
             return View();
         }
---
+++ SalesWebMvc/Services/SellersService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Services
+{
+    public class SellersService
+    {
+
+        private readonly SalesWebMvcContext _context;
+        public SellersService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+        public List<Sellers> FindAll()
+        {
+            return _context.Sellers.ToList();
+        }
+        public void Insert(Sellers obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+    }
+}
