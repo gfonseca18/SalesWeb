2422d0de11771db3337deebc8ea4bd4d5f7dced5
gfonseca18 <gercia.fonseca@icloud.com>
2021-06-06 22:50:12 +0100

    Delete Seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SalesWebMvc.Models;
 using SalesWebMvc.Services;
+using SalesWebMvc.Models.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -13,10 +14,12 @@ namespace SalesWebMvc.Controllers
     public class SellersController : Controller
     {
         private readonly SellersService _sellersService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellersService sellerService)
+        public SellersController(SellersService sellerService,DepartmentService departmentService)
         {
             _sellersService = sellerService;
+            _departmentService = departmentService;
         }
         // GET: /<controller>/
         public IActionResult Index()
@@ -26,7 +29,9 @@ namespace SalesWebMvc.Controllers
         }
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
         [HttpPost]//anottation
         [ValidateAntiForgeryToken]
@@ -35,6 +40,19 @@ namespace SalesWebMvc.Controllers
             _sellersService.Insert(sellers);
             return RedirectToAction(nameof(Index));
 
+        }
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellersService.FindById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
         }
     }
 }
---
+++ SalesWebMvc/Models/ViewModels/SellerFormViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Models.ViewModels
+{
+    public class SellerFormViewModel
+    {
+        public Sellers Sellers { get; set; }
+        public ICollection<Department> Departments { get; set; }
+
+        
+    }
+}
